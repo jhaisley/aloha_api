866c51cc8836abf78de8edb4830824941d78c857
Jordan Haisley <jordanhaisley@gmail.com>
2025-03-11 09:10:36 -0400

    Initial commit: Aloha API Python client

---
+++ __init__.py
@@ -0,0 +1,42 @@
+"""
+Aloha API Client
+
+A Python package for interacting with the Aloha ABA Practice Management Software API.
+"""
+
+__version__ = "0.1.0"
+
+from .api import (
+    get_access_token,
+    refresh_access_token,
+    list_appointments,
+    list_clients,
+    list_authorizations,
+    list_billing_ledger,
+    list_authorizations_without_appointments,
+    BASE_URL,
+    CLIENT_ID,
+    # Export exception classes
+    AlohaApiError,
+    AuthenticationError,
+    ConfigurationError,
+    ApiRequestError
+)
+
+__all__ = [
+    'get_access_token',
+    'refresh_access_token',
+    'list_appointments',
+    'list_clients',
+    'list_authorizations',
+    'list_billing_ledger',
+    'list_authorizations_without_appointments',
+    'BASE_URL',
+    'CLIENT_ID',
+    # Exception classes
+    'AlohaApiError',
+    'AuthenticationError',
+    'ConfigurationError',
+    'ApiRequestError'
+]
+
